607df2cdb392caaacbfdce49181f854e150c0349
Firoz Thakur <firozbhaikardar21@gmail.com>
2022-03-04 09:39:55 +0530

    new change

--- testing/src/test/java/ui/OrangeHRMTestcase.java
+++ testing/src/test/java/ui/OrangeHRMTestcase.java
@@ -7,8 +7,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class OrangeHRMTestcase {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://opensource-demo.orangehrmlive.com/");
