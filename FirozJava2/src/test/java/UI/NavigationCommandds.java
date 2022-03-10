47f374e52b7c50e577e3173b13417f8f28b748e4
Firoz Thakur <firozbhaikardar21@gmail.com>
2022-03-10 16:25:20 +0530

    hye

---
+++ FirozJava2/src/test/java/UI/NavigationCommandds.java
@@ -0,0 +1,28 @@
+package UI;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class NavigationCommandds {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// TODO Auto-generated method stub
+		
+				WebDriverManager.chromedriver().setup();
+				ChromeDriver driver = new ChromeDriver();
+				driver.get("https://www.facebook.com/");
+				System.out.println(driver.getTitle());
+				driver.navigate().to("https://www.instagram.com/");
+				System.out.println(driver.getTitle());
+				driver.navigate().back();
+				System.out.println(driver.getTitle());
+				driver.navigate().forward();
+				driver.navigate().refresh();
+				driver.close();
+				
+	}
+
+}
---
+++ FirozJava2/src/test/java/UI/OrangeHRMtestcase.java
@@ -0,0 +1,10 @@
+package UI;
+
+public class OrangeHRMtestcase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
